7063fb36b4000091dd43377c513adf58d1e1e7ea
Marco Aurelio Cardoso <marcoaurelio.cardoso@gmail.com>
2024-02-17 10:42:43 +0000

    chore: init project

---
+++ src/test/java/br/ufes/sead/erp/ErpApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufes.sead.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
